82ecda1a89f97fbe7bb22816b50f0470f9395aa2
Aneez Muhammed <aneezani666@gmail.com>
2024-01-02 23:55:41 +0530

    connected mongodb using moongoose

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const mongoose = require('mongoose');
 const productroutes = require('./routes/productroutes')
 const categoryroutes = require('./routes/categoryroutes')
 
@@ -12,7 +14,18 @@ app.use('/products', productroutes)
 app.use('/categories', categoryroutes)
 
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+main()
+.then(()=> console.log("DB Connected"))
+.catch(err => console.log(err));
+
+async function main() {
+    const url = process.env.DB_URL
+    const password = process.env.DB_PASSWORD
+    const url_password = url.replace('<password>', password)  // creating variable for password included url
+    
+  await mongoose.connect(url_password);
+}
