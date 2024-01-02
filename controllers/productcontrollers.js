d8996bb015e013dc8289346783aea91de8e8b892
Aneez Muhammed <aneezani666@gmail.com>
2024-01-03 01:22:54 +0530

    mongoose connected, route added

--- controllers/productcontrollers.js
+++ controllers/productcontrollers.js
@@ -1,17 +1,50 @@
+const Product = require("../models/productmodel");
+
 const getAllproducts = async(req, res) => {
-    res.status(401).send("not written")
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products) 
+    } catch (error) {
+        res.status(500).send("Server Error")
+    }
 }
 const getProductsbyId = async(req, res) => {
-    res.status(401).send("not written")
+    try {
+        const products= await Product.findById(req.params.productId).exec();
+        res.status(200).json(products)
+
+    } catch (error) {
+        res.status(404).send("not found")
+    }
 }
 const addProducts = async(req, res) => {
-    res.status(401).send("not written")
+    try {
+        const product = await new Product(req.body);
+        await product.save()
+        res.status(201).json(product)
+    } catch (error) {
+        res.status(401).send("Check product fields")
+    }
 }
 const updateProducts = async(req, res) => {
-    res.status(401).send("not written")
+    try {
+        const updatedproduct = await Product.findByIdAndUpdate(req.params.productId, req.body , {new:true})
+        res.status(201).json(updatedproduct)
+
+    } catch (error) {
+        res.status(400).send("product not found")
+        
+    }
 }
 const deleteProducts = async(req, res) => {
-    res.status(401).send("not written")
+    try {
+       await Product.findByIdAndDelete(req.params.productId)
+       res.status(201).send("deleted")
+        
+    } catch (error) {
+       res.status(400).send("product not found")
+        
+    }
 }
 
 module.exports = {
